f96c7341dbd59a713e079cd437b6a12d586a7a67
pablo <pablo.reyes@udec.cl>
2026-04-27 00:06:54 -0400

    check results

--- run_twins.bash
+++ run_twins.bash
@@ -4,11 +4,11 @@
 #SBATCH -p general
 #SBATCH -n 1
 #SBATCH --ntasks-per-node=1
-#SBATCH -c 10
-#SBATCH --mem-per-cpu=2000
+#SBATCH -c 40
+#SBATCH --mem-per-cpu=1200
 #SBATCH --mail-user=pareyes2018@udec.cl
 #SBATCH --mail-type=ALL
-#SBATCH -t 1-0:0:0
+#SBATCH -t 0-12:0:0
 #SBATCH -o enut-ii/twins_%A_%a.err.out
 #SBATCH -e enut-ii/twins_%A_%a.err.out
 
